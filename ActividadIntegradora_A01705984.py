aab8f2c15f4ff389fdb8f00223a1a40f1bd54974
diegarza <a01705984@tec.mx>
2023-11-24 17:45:03 -0600

    5cambios

--- ActividadIntegradora_A01705984.py
+++ ActividadIntegradora_A01705984.py
@@ -67,7 +67,8 @@ st.bar_chart(subset_data['Day'].value_counts())
 st.markdown('Crimes ocurred per date')
 st.bar_chart(subset_data['Date'].value_counts())
 st.markdown('Types of crimes committed')
-st.bar_chart(subset_data['Incident Category'].value_counts())
+st.bar_chart(subset_data['Incident Category'].value_counts(),
+             color = (.7,.3,.6,.5))
 
 agree = st.button('Click to see incident subcategories')
 if agree:
